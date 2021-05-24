576838fe27b8eced30b27c75518d64bdce1e08f8
Harshi <72039632+Harshi9843@users.noreply.github.com>
2021-05-24 18:22:50 +0100

    Add files via upload

---
+++ SettingScreen.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+
+export default class SettingScreen extends Component{
+    render(){
+        return(
+            <View>
+                          
+            </View>
+        )
+    }
+}
